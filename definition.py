116ef8d5a8d1f69fd4b72e2956bf6be1ffef03e4
Owner <joywin16@gmail.com>
2023-11-16 15:36:32 +0000

    Defination class build and tested fully working

--- definition.py
+++ definition.py
@@ -1,8 +1,23 @@
+import pandas
+
 class Defination:
 
     def __init__(self, term):
-        self.term = term
+        self.term = term.strip()
 
     def get(self):
-        pass
+        df = pandas.read_csv("data.csv")
+        # tuple since the defination can be more then one
+        result = tuple(df.loc[df['word'] == self.term]['definition'])
+        return result
+
+
+if __name__ == "__main__":
+    definition = Defination(term='lot')
+    print(definition.get())
+
+
+
+
+
 
