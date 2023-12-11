6830b3b05acf313619a2c3b67ac511018ef46b25
Joywin Gonsalves <38892713+thedowntownboy@users.noreply.github.com>
2023-12-11 18:35:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import justpy as jp
 import uvicorn
 
-app = jp.app 
-
 from webapp.about import About
 from webapp.dictionary import Dictionary
 from webapp.home import Home
 
-# jp.Route(About.path, About.serve)
+app = jp.app 
+jp.Route(About.path, About.serve)
 jp.Route(Home.path, Home.serve)
-# jp.Route(Dictionary.path, Dictionary.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
 jp.justpy(start_server=False)
 uvicorn.run(app)
